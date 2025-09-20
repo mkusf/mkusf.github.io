11c39752390235f368bda2b76864699caddbe2c4
mkusf <107431102+mkusf@users.noreply.github.com>
2025-09-20 16:22:33 -0700

    Add favicon and minimal navbar for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-
+import Nav from './components/NavBar'
 function App() {
   return (
-    <div className='container-page-padding'>
-      <div className='flex gap-3'>
-        <div className='small-box-placeholder'> </div>
-        <div> projects </div>
-        <div> contact </div>
-        <div> bio </div>
-        <div> resume </div>
-      </div>
+    <div className='flex'>
+      <Nav></Nav>
+      
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+import menuIcon from "/assets/menu-icon.png";
+import closeIcon from "/assets/grp4.png";
+
+
 export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <div className="">
-            
+        <div className='flex justify-between items-center gap-3 bg-[#1E1E1E] w-screen px-2.5 '>
+            <img className = " w-18 h-18" alt="Website Icon" src="/assets/mkusf-web-icon-removebg.png"></img>
+            <img
+                src={isOpen ? closeIcon : menuIcon}
+                alt="menu toggle"
+                className="w-7 h-6 mr-4 bg-cover cursor-pointer"
+                onClick={() => setIsOpen(!isOpen)}
+            />
         </div>
     );
 }
