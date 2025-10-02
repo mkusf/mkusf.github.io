3f0ff2387a978d177c7d39bbff767c7ef6fb7874
mkusf <107431102+mkusf@users.noreply.github.com>
2025-10-01 19:38:44 -0700

    Add information to projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -7,6 +7,15 @@ export default function Projects({ id }) {
     const [leftArrow, setLeftArrow] = useState("assets/left.png");
     const [project, setProject] = useState(0);
     const project_total = 2;
+    const techStack = [
+    { src: "assets/Git-Icon-White.svg", alt: "Git logo" },
+    { src: "assets/Github_logo.svg", alt: "GitHub logo" },
+    { src: "assets/React-icon.svg", alt: "React logo" },
+    { src: "assets/Javascript_Logo.png", alt: "JavaScript logo" },
+    { src: "assets/tailwind.svg", alt: "TailwindCSS logo" },
+    { src: "assets/jest.svg", alt: "Jest logo" },
+    { src: "assets/render_logo.svg", alt: "Render logo" },
+    ];
 
     function changeProject(newState) {
         const project_index = (newState + project_total) % project_total;
@@ -14,12 +23,12 @@ export default function Projects({ id }) {
     }
     
     return(
-        <div id={id} className="flex flex-col">
+        <div id={id} className="flex flex-col gap-5">
             <div className="mt-20 font-light text-5xl text-center">
                 Projects
             </div>
             {/* Selector */}
-            <div className="rounded-3xl mx-3 flex justify-center gap-2 mt-5">
+            <div className="rounded-3xl mx-3 flex justify-center gap-2">
                 <img 
                     onClick={() => changeProject(project - 1)}
                     onMouseEnter={() => setLeftArrow("assets/left-hover.png")}
@@ -32,7 +41,7 @@ export default function Projects({ id }) {
                     src={leftArrow} alt="Left arrow button"
                 />
                 {/* Project Titles */}
-                <div className="bg-[#1E1E1E] w-40 rounded-xl relative">
+                <div className="bg-[#1E1E1E] w-40 rounded-xl relative py-5">
                     <div
                         className={`
                             absolute inset-0 flex items-center justify-center
@@ -68,9 +77,49 @@ export default function Projects({ id }) {
                     src={rightArrow} alt="Right arrow button"
                 />
             </div>
-            <div className={`flex-row flex justify-center mt-2 gap-2`}>
+            {/* Shows which project is selected*/}
+            <div className={`flex-row flex justify-center gap-2`}>
                 <div className={project === 0 ? `bg-[#F3F4F6] w-3 h-3 rounded-full` : `bg-[#3D3D3D] w-3 h-3 rounded-full`}></div>
                 <div className={project === 1 ? `bg-[#F3F4F6] w-3 h-3 rounded-full` : `bg-[#3D3D3D] w-3 h-3 rounded-full`}></div>
+            </div>
+            {/* 
+            Text of Project:
+            Icon
+            Body text
+            */}
+            <div className="bg-[#1E1E1E] min-h-20 rounded-2xl mx-3 p-5 flex flex-col">
+                {/* Body TripTips */}
+                {project === 0 &&
+                    <div className="flex flex-col items-center gap-2">
+                        <div className="flex flex-row flex-wrap gap-3 justify-center">
+                            {/* icons of techstack */}
+                            {techStack.map((tech, idx) => (
+                                <img
+                                    key={idx}
+                                    src={tech.src}
+                                    alt={tech.alt}
+                                    className="object-contain w-10 h-10"
+                                />
+                            ))}
+                        </div>
+                        <a href="https://final-project-4-winds.onrender.com" className="text-[#8B5CF6] hover:text-[#14B8A6] active:text-[#14B8A6]">Link to Website</a>
+                        <div className="flex flex-col gap-2">
+                            <div className="self-start font-light text-base">
+                                Trip Tips is a team project for SFU's CMPT276 course. 
+                                Developed along side three peers, this project was created to execute a Agile software development life cycle using mix of Scrum and KanBan.
+                            </div>
+                            <div>
+                                Planning
+                            </div>
+                            <div>
+                                Development
+                            </div>
+                            <div>
+                                CI/CD
+                            </div>
+                        </div>
+                    </div>
+                }
             </div>
         </div>
     );
